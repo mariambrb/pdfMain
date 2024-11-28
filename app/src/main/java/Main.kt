25bb7fdcc19743c50f3c17ac7d92a235742cfa2a
mariambrb <mariambarbakadze42@gmail.com>
2024-11-28 22:28:08 +0400

    Update Main.kt

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,32 +1,34 @@
-fun main(){
-var answer=""
-    do{
-        print("insert value of X:")
-        var x=readLine()
-        print("insert value of Y:")
-        var y=readLine()
-        var fixedX=x?.filter { it.isDigit()}?.toIntOrNull()?:0
-        var fixedY=y?.filter { it.isDigit()}?.toIntOrNull()?:0
+fun main() {
+    var answer = ""
+    do {
+        if (answer != "N") {
+            print("insert value of X:")
+            val x = readLine()
+            print("insert value of Y:")
+            val y = readLine()
+            val fixedX = x?.filter { it.isDigit() }?.toIntOrNull() ?: 0
+            val fixedY = y?.filter { it.isDigit() }?.toIntOrNull() ?: 0
 
-        if(fixedY!=0){
-            var z=fixedX.toFloat()/fixedY.toFloat()
-            println("${fixedX} divided by ${fixedY} = ${if (z % 1==0f)z.toInt()else z}")
-        }else{
-            println("can't divide by 0")
+            if (fixedY != 0) {
+                val z = fixedX.toFloat() / fixedY.toFloat()
+                println("${fixedX} divided by ${fixedY} = ${if (z % 1 == 0f) z.toInt() else z}")
+            } else {
+                println("can't divide by 0")
+            }
         }
-    do{ print("insert Y/N only!")
-        var yesOrNo=readLine()?.toUpperCase()
+        do {
+            print("გსურთ პროგრამის ხელახლა დაწყება ? insert Y/N only!")
+            val yesOrNo = readLine()?.toUpperCase()
 
-        if(yesOrNo=="Y"){
-          answer = yesOrNo
-        }
-       else if ( yesOrNo == "N"){
-        println("The End")
-       }
-       }while( answer!="Y" && answer!="N")
+            if (yesOrNo == "Y") {
+                answer = yesOrNo
+            } else if (yesOrNo == "N") {
+                println("The End")
+                answer = yesOrNo
+            }
+        } while (answer != "Y" && answer != "N")
 
-
-    }while(answer=="Y")
+    } while (answer == "Y")
 }
 
 
